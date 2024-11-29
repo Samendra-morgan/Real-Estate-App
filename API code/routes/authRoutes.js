a023debe2a0303ff2b90c1f5e1206ba895d1efef
Samendramorgan <morgansamendra@gmail.com>
2024-11-29 17:05:45 +0200

    Changes

--- API code/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const { signup, signin } = require('../controller/authController')
-
-router.post('/signup', signup);
-
-router.post('/signin', signin);
-
-module.exports = router
